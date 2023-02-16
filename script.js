294adab0705a1370c79f1da7931b05536573f767
Mohd Faizan <42490239+Mohdfaizan-3@users.noreply.github.com>
2023-02-16 16:50:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+
+const form = document.querySelector("#form");
+const display = document.getElementById("result");
+
+form.addEventListener("submit", submit);
+
+function submit(e) {
+  e.preventDefault();
+
+  const input = document.querySelector("input").value;
+	
+  let arr = input.split(",");
+   arr.sort((a,b)=>{return a-b;});
+	//arr.sort()
+  
+  let cost = 0;
+
+  while (arr.length > 1) {
+    let sum = Number(arr[0]) + Number(arr[1]);
+	      arr.push(sum);
+    cost += sum;
+    arr.splice(0, 2);
+
+    
+ arr.sort((a,b)=>{return a-b;});	  
+  }
+	document.querySelector("input").value = null;
+
+	display.textContent = cost;
+}
